fc693cada7660e42b5a852d47bf9714fe69c73b2
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2024-02-17 12:03:01 +0530

    Create index.js

---
+++ words with vowels/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString.trim().split("\n").map((str) => str.trim());
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  const wordsList = JSON.parse(readLine().replace(/'/g, '"'));
+  const vowelsList = ["a", "e", "i", "o", "u"];
+let output=wordsList.filter((word)=>{
+    for(let char in wordsList){
+        if(word.toLowerCase().indexOf(vowelsList[char])>=0){
+            return word;
+        }
+        
+    }
+});
+console.log(output)
+/* Please do not modify anything above this line */
+
+  // Write your code here
+  
+}
